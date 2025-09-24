0935d8bbf93d01c6c61f52466aad5f7e85ce40f3
Brendon McCool <brendonmccool@gmail.com>
2025-09-24 15:56:18 -0400

    Restore new project

---
+++ src/draw.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+void draw(){
+    printf("draw\n");
+}
+
+void render(){
+    printf("render\n");
+}
+
---
+++ test_package/src/example.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "draw.h"
+
+
+int main() {
+    printf("Example using draw library:\n");
+    draw();
+    render();
+    return 0;
+}
